7d6a696a9a462b2930891df58d35820b8d22be05
frblazqu <frblazqu@ucm.es>
2018-05-20 21:46:03 +0200

    Eliminados dudas en código y deprecateds.

--- src/main/java/es/ucm/fdi/launcher/ExampleMain.java
+++ src/main/java/es/ucm/fdi/launcher/ExampleMain.java
@@ -42,19 +42,6 @@ public class ExampleMain {
 														// escribir los datos
 	public static String _simMode = DEFAULT_SIM_MODE; // Modo de ejecución
 
-
-	/**
-	 * DUDAS:
-	 *
-	 * ¿Por qué parsea un array y no un string? ¿Presupone split? ¿Clase
-	 * Options? ¿Clase CommandLineParser? ¿Clase CommandLine?
-	 *
-	 * Ya no es que me interese saber exactamente que hacen estos métodos pero
-	 * si que quiero saber por qúe no se ha tenido que implementar el parser, lo
-	 * ha cogido ya hecho. ¿De dónde lo ha sacado? ¿Cómo puedo verlo? En
-	 * definitiva buscar las librerías org.apache.commons y ver que se cuece por
-	 * ahí. ¿Se puede hacer esto sin maven? Dudas para el bueno de ManuFreire.
-	 */
 	public static void parseArgs(String[] args) {
 		// Si hay algún error de parseo hace que todo termine (ver catch final)
 		
